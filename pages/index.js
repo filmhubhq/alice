868622aed3444c1a676f5bdf204b50b1bb04c10a
Klaus Badelt <info@klausbadelt.com>
2021-06-15 23:09:11 -0700

    copy from popsy

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Popsy</title>
+        <meta name="description" content="Welcome to Popsy" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.title}>
+          <Image
+            src="/popsy.png"
+            alt="Popsy Logo"
+            width={400}
+            height={258/2}
+          />
+        </div>
+
+        <p className={styles.description}>
+          Stream on
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://plex.tv" className={styles.card}>
+            <Image
+              src="/plex.png"
+              alt="Plex Logo"
+              width={151}
+              height={55}
+            />
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://filmhub.com?utm_source=popsy&utm_medium=web&utm_campaign=popsy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Filmmaker? Submit via&nbsp;
+          <span className={styles.logo}>
+            <Image src="/filmhub-dark.png" alt="Filmhub Logo" width={1200/9} height={128/9} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
